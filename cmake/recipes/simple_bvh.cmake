10ee26c8e5ee3976e3458723083da02992b8d93f
M-axssi <290516872@qq.com>
2025-03-18 20:56:33 +0800

    .

--- cmake/recipes/simple_bvh.cmake
+++ cmake/recipes/simple_bvh.cmake
@@ -8,4 +8,8 @@ endif()
 message(STATUS "Third-party: creating target 'simple_bvh::simple_bvh'")
 
 include(CPM)
-CPMAddPackage("gh:M-axssi/SimpleBVH#db244ebdd4ea348849a9f2b6ef7e908ddbf75ff4")
+CPMAddPackage(
+    NAME SimpleBVH
+    GIT_REPOSITORY "git@github.com:M-axssi/SimpleBVH.git"
+    GIT_TAG "db244ebdd4ea348849a9f2b6ef7e908ddbf75ff4"
+)
